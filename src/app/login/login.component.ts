8cc16cb9334c53b9eade3548a89f43d25cfd81ea
Laz <lazgerasimov@gmail.com>
2022-12-09 09:37:56 +0000

    update login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+
+  handleFormSubmit(value: {email: string; password: string}): void {
+    console.log(value);
+  }
 }
