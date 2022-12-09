b526b807580a96f3836a85ade0bc2791264e2035
Laz <lazgerasimov@gmail.com>
2022-12-09 09:10:57 +0000

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { MyRouterLinkDirective } from './my-router-link.directive';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
