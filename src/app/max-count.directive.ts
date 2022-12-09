098165fe639f799c94c062431b7aff194150675c
Laz <lazgerasimov@gmail.com>
2022-12-09 10:29:44 +0000

    .

--- src/app/max-count.directive.ts
+++ src/app/max-count.directive.ts
@@ -1,19 +1,35 @@
-import { Directive } from '@angular/core';
-import { AbstractControl, ValidationErrors, Validator } from '@angular/forms';
+import { Directive, Input } from '@angular/core';
+import { AbstractControl, NG_VALIDATORS, ValidationErrors, Validator } from '@angular/forms';
 
 @Directive({
-  selector: '[appMaxCount]'
+  selector: '[appMaxCount]',
+  providers: [
+    {
+      provide: NG_VALIDATORS,
+      useExisting: MaxCountDirective,
+      multi: true
+    }
+  ]
 })
 export class MaxCountDirective implements Validator {
+
+  @Input() appMaxCount: number | undefined;
 
   constructor() { }
 
   validate(control: AbstractControl<any, any>): ValidationErrors | null {
-    throw new Error('Method not implemented')
+    if (this.appMaxCount === undefined
+      || control.value?.length === 0 
+      || control.value?.length || 0 <= this.appMaxCount) { return null;  }
+    return {
+      appMaxCount: this.appMaxCount
+    }
   }
 
-  registerOnValidatorChange(fn: () => void): void {
-    throw new Error('Method not implemented')
-  }
+
+
+  // registerOnValidatorChange?(fn: () => void): void {
+  //   throw new Error('Method not implemented')
+  // }
 
 }
