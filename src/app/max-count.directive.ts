6092abd19f1d87da01980b4be492a3ca72ffd3d3
Laz <lazgerasimov@gmail.com>
2022-12-09 10:09:19 +0000

    add custom validator

--- src/app/max-count.directive.ts
+++ src/app/max-count.directive.ts
@@ -1,10 +1,19 @@
 import { Directive } from '@angular/core';
+import { AbstractControl, ValidationErrors, Validator } from '@angular/forms';
 
 @Directive({
   selector: '[appMaxCount]'
 })
-export class MaxCountDirective {
+export class MaxCountDirective implements Validator {
 
   constructor() { }
+
+  validate(control: AbstractControl<any, any>): ValidationErrors | null {
+    throw new Error('Method not implemented')
+  }
+
+  registerOnValidatorChange(fn: () => void): void {
+    throw new Error('Method not implemented')
+  }
 
 }
