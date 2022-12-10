90c632ecc02a983c9012f2560958af4425585a39
Laz <lazgerasimov@gmail.com>
2022-12-10 11:27:16 +0000

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+      this.userService.getUsers().subscribe({
+        next: (users) => console.log(users),
+        error: (err) => console.error(err)
+      })
+  }
   title = 'angular-forms';
 }
--- src/app/app.interceptor.ts
+++ src/app/app.interceptor.ts
@@ -2,11 +2,21 @@ import { Injectable, Provider } from "@angular/core";
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse, HTTP_INTERCEPTORS} from '@angular/common/http';
 import { mergeMap, Observable, tap } from "rxjs";
 
+import {environment } from '../environments/environment';
+
+const apiUrl = environment.apiUrl;
+
 
 @Injectable()
 export class AppInterceptor implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-       return next.handle(req).pipe(
+        let request = req;
+        if (request.url.startsWith('/api')) {
+            request = req.clone({
+                url: req.url.replace('/api', apiUrl)
+            })
+        }
+       return next.handle(request).pipe(
         tap((req) => {
             if (req instanceof HttpResponse) {
                 console.log(req.body);
